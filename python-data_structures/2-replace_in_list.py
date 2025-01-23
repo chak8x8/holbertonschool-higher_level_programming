71669ad44b43a4588e67a339f0c1685188a1af09
chak8x8 <chak8x8@gmail.com>
2025-01-23 21:25:47 +1100

    3. Print a list of integers... in reverse!

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,10 +2,8 @@
 def replace_in_list(my_list, idx, element):
     size = len(my_list)
 
-    # Check for invalid index
     if idx < 0 or idx >= size:
         return my_list
 
-    # Replace the element at the specified index
     my_list[idx] = element
     return my_list
