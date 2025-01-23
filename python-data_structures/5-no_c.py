fa9cc325dd2594f151214b4871a0e80d1f58bab3
chak8x8 <chak8x8@gmail.com>
2025-01-23 21:52:33 +1100

    5. Can you C me now?

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    
+    # Create a translation table to remove 'c' and 'C'
+    translation_table = {ord('c'): None, ord('C'): None}
+
+    # Use translate() with the translation table
+    my_string = my_string.translate(translation_table)
+    return my_string
