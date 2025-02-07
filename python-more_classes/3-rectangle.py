214e5e0ef07035b1a386eb9afb29361f6ab5510a
chak8x8 <chak8x8@gmail.com>
2025-02-07 20:07:49 +1100

    3. String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -20,11 +20,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -35,19 +35,19 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
-        
+
     def __str__(self):
         """Returns a string representation of the rectangle with #"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         rectangle = []
         for _ in range(self.__height):
             rectangle.append("#" * self.__width)
-            
+
         return "\n".join(rectangle)  # Joins all rows with a newline
