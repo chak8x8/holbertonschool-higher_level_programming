3822ef88ce61a9f6b4d05b21b40ac3f4effca533
chak8x8 <chak8x8@gmail.com>
2025-02-11 20:15:12 +1100

    1. My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,6 @@ class MyList(list):
     """A subclass of list that includes a method to print a sorted list."""
 
     def print_sorted(self):
-        """Prints the list in ascending sorted order without modifying the original list."""
-        print(sorted(self))  # sorted() returns a new sorted list, does not modify the original list
+        """Prints the list in ascending sorted order
+        without modifying the original list."""
+        print(sorted(self))
