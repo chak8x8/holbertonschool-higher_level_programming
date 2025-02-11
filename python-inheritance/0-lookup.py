70e3528fdec864b02575609ca4060b93ce0528ab
chak8x8 <chak8x8@gmail.com>
2025-02-11 19:35:53 +1100

    0. Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines a function that returns the list of available attributes and methods of an object."""
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
+    return dir(obj)
