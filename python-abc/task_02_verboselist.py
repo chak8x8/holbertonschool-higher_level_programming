f76ff1b04dd8560557cb295f2258e9fbf147b57a
chak8x8 <chak8x8@gmail.com>
2025-02-14 20:43:27 +1100

    2. Extending the Python List with Notifications

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,21 +1,53 @@
 #!/usr/bin/python3
+"""
+Define VerboseList, a class that extends Python's built-in list with extra
+notifications whenever items are added or removed.
+"""
 
 class VerboseList(list):
+    """
+    Extends Python's built-in list to print notification messages
+    whenever certain operations modify the list:
+      - append
+      - extend
+      - remove
+      - pop
+    """
+
     def append(self, item):
+        """
+        Append an item to the end of the list, then print a message
+        indicating which item was added.
+        """
         super().append(item)
         print("Added [{}] to the list.".format(item))
 
     def extend(self, iterable):
+        """
+        Extend the list by appending elements from the given iterable,
+        then print how many items were added.
+        """
         super().extend(iterable)
         print("Extended the list with [{}] items.".format(len(iterable)))
 
     def remove(self, item):
+        """
+        Remove the first occurrence of the specified item from the list,
+        printing a message before removal.
+        """
         print("Removed [{}] from the list.".format(item))
         super().remove(item)
-        
-    def pop(self, index = None):
-        if index is None:
-            pop_item = super().pop()
-        else:
-            pop_item = super().pop(index)
-        print("Popped [{}] from the list.".format(popped_item))
+
+    def pop(self, index=None):
+        """
+        Remove and return the item at the specified index (defaulting to
+        the last item if no index is provided), then print a message
+        indicating which item was popped.
+        """
+        def pop(self, index=None):
+            if index is None:
+                popped_item = super().pop()       # pop the last item and store it
+            else:
+                popped_item = super().pop(index) # pop at 'index' and store it
+            print("Popped [{}] from the list.".format(popped_item))
+
