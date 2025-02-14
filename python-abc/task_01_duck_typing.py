9a091c5beee8c529f02473bd94b32cc890730803
chak8x8 <chak8x8@gmail.com>
2025-02-14 19:52:09 +1100

    1. Shapes, Interfaces, and Duck Typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -22,6 +22,8 @@ class Circle(Shape):
     """Concrete class representing a Circle"""
 
     def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius must be greater than 0")
         self.__radius = radius
 
     def area(self):
