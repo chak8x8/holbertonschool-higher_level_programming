ab387451a6069cd3e4ee7717cdecac3198e3ee0d
chak8x8 <chak8x8@gmail.com>
2025-02-20 18:21:59 +1100

    7. Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """Script that adds all command-line arguments to a list and saves them to a JSON file"""
 
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from 5-save_to_json_file.py import save_to_json_file
+from 6-load_from_json_file.py import load_from_json_file
 
 filename = "add_item.json"
 
