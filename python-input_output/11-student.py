327b1e8be064e5d820f959459c8b8abd7709716c
chak8x8 <chak8x8@gmail.com>
2025-02-21 18:25:50 +1100

    11. Student to disk and reload

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -32,12 +32,16 @@ class Student:
             dict: A dictionary representation of the student.
         """
         if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+            }
         return self.__dict__
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance 
+        Replaces all attributes of the Student instance
         based on the provided dictionary.
 
         Args:
