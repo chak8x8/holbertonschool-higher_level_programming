2ef58cae70f3ab83c50080e0713748078749af63
chak8x8 <chak8x8@gmail.com>
2025-02-24 19:35:08 +1100

    12. Pascal's Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Generates Pascal's Triangle up to n rows."""
 
+
 def pascal_triangle(n):
     """Returns a list of lists representing Pascal’s Triangle.
 
@@ -20,7 +21,7 @@ def pascal_triangle(n):
         new_row = [1]  # Every row starts with 1
 
         # Compute middle elements
-        for j in range(1, len(prev_row)):  
+        for j in range(1, len(prev_row)):
             new_row.append(prev_row[j - 1] + prev_row[j])
 
         new_row.append(1)  # Every row ends with 1
