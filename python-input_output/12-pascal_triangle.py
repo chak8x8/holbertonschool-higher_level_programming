425cb7390696fcf4b5c41a659e26790b192c8df5
chak8x8 <chak8x8@gmail.com>
2025-02-24 19:29:03 +1100

    12. Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    list = []
+    if n == 0:
+        return list
+    for i in range(1:n):
+        inner_list = []
+        for j in range(i:n):
+        inner_list.extend(
+            
+        )
+        list.extend(inner_list)
+    
