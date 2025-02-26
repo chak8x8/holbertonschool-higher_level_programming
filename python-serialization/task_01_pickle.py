3a5fae1b88654603b7d31498a66ec8a3d75c6956
chak8x8 <chak8x8@gmail.com>
2025-02-27 02:46:36 +1100

    1. Pickling Custom Classes

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -52,6 +52,6 @@ class CustomObject:
         try:
             with open(filename, "rb") as file:
                 return pickle.load(file)
-        except (FileNotFoundError, pickle.UnpicklingError) as e:
+        except (FileNotFoundError, pickle.UnpicklingError, EOFError) as e:
             print(f"Error loading from file {filename}: {e}")
             return None
