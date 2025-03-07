7f10e2b78be548cc453d5ec2c780f85bf865aaab
chak8x8 <chak8x8@gmail.com>
2025-03-07 20:04:59 +1100

    3. Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -10,12 +10,14 @@ class SimpleAPI(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
+
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode("utf-8"))
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
