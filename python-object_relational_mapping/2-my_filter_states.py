2d0a5010f4c2cde4a3fac0b1a69bb4013ded98a5
chak8x8 <chak8x8@gmail.com>
2025-03-29 18:11:30 +1100

    2. Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # Correct query + call execute()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
+    query = "SELECT * FROM states WHERE name = '{}' " \
+        "ORDER BY id ASC;".format(state_name)
     cur.execute(query)
 
     # Correct fetchall()
